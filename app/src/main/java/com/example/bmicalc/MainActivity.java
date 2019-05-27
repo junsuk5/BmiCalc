1ab60cd7f010c680a6f72aa74b7c1c28e0243d13
miyoung <0narara0@gmail.com>
2019-05-27 15:11:15 +0900

    nav_graph에  ResultFragment 넣고 argument 추가

--- app/src/main/java/com/example/bmicalc/MainActivity.java
+++ app/src/main/java/com/example/bmicalc/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.bmicalc;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NavController mNavController;
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mNavController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+
     }
+
 }
