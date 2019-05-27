ea151db6b018f1977b7b663208cca8d01c3d0d6e
Junsuk Oh <a811219@gmail.com>
2019-05-27 15:47:37 +0900

    caculate 메서드 인자 순서 변경

--- app/src/main/java/com/example/bmicalc/BmiCalculator.java
+++ app/src/main/java/com/example/bmicalc/BmiCalculator.java
@@ -4,11 +4,12 @@ public class BmiCalculator implements Calculator {
 
     /**
      * Bmi 값을 얻는다
-     * @param weight 몸무게 (kg)
+     *
      * @param height 키 (cm)
+     * @param weight 몸무게 (kg)
      * @return Bmi
      */
-    public float calculate(float weight, float height) {
-        return  (float) (weight / Math.pow(height / 100.0, 2.0));
+    public float calculate(float height, float weight) {
+        return (float) (weight / Math.pow(height / 100.0, 2.0));
     }
 }
