d715ea6a87b2b10deac38821986f0b2ede1f9861
miyoung <0narara0@gmail.com>
2019-05-27 21:21:31 +0900

    MainActivity ActionBar에  NavigationUI로 NavController과 연동

--- app/src/main/java/com/example/bmicalc/MainActivity.java
+++ app/src/main/java/com/example/bmicalc/MainActivity.java
@@ -1,19 +1,15 @@
 package com.example.bmicalc;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
     private NavController mNavController;
-    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +17,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mNavController = Navigation.findNavController(this, R.id.nav_host_fragment);
-
+        NavigationUI.setupActionBarWithNavController(this, mNavController);
 
     }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return mNavController.navigateUp() ||super.onSupportNavigateUp();
+    }
+
 
 }
--- app/src/main/java/com/example/bmicalc/ResultFragment.kt
+++ app/src/main/java/com/example/bmicalc/ResultFragment.kt
@@ -24,14 +24,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import kotlinx.android.synthetic.main.fragment_result.*
 
 
 class ResultFragment : Fragment() {
+    val args: ResultFragmentArgs by navArgs()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_result, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val calculator = BmiCalculator()
+        val bmi = calculator.calculate(args.height, args.weight)
+
+        when {
+            bmi >= 35 -> result_text.text = "고도 비만"
+            bmi >= 30 -> result_text.text = "2단계 비만"
+            bmi >= 25 -> result_text.text = "1단계 비만"
+            bmi >= 23 -> result_text.text = "과체중"
+            bmi >= 18.5 -> result_text.text = "정상"
+            else -> result_text.text = "저체중"
+        }
+
+
     }
 
 
